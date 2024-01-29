64cb72a2ecc7f9c9e8eada767bf57e064a45e850
osoff <anatolywebdev@gmail.com>
2024-01-29 01:28:41 +0100

    fix:avatar

--- src/ui/Avatar.jsx
+++ src/ui/Avatar.jsx
@@ -1,5 +1,5 @@
 export default function Avatar() {
   return (
-    <div className="av w-[10rem] h-[10rem] sm:h-[20rem] sm:w-[20rem] md:w-[30rem] md:h-[30rem]"></div>
+    <div className=" bg-avatar av w-[10rem] h-[10rem] sm:h-[20rem] sm:w-[20rem] md:w-[30rem] md:h-[30rem]"></div>
   );
 }
