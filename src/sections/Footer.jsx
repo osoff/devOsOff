c25f4ace2f1b7c10604a59ad2651bb6230fed469
osoff <anatolywebdev@gmail.com>
2024-01-29 01:40:47 +0100

    sdfsdfs

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,7 @@
 import { FaGithub } from "react-icons/fa6";
 import { FaTelegram } from "react-icons/fa6";
 import { IoMail } from "react-icons/io5";
+import { linksOutside } from "../constants";
 
 function Footer() {
   const year = new Date().getUTCFullYear();
@@ -9,9 +10,30 @@ function Footer() {
       <div className="flex justify-between items-center">
         <p>Copyright © {year}. All rights are reserved</p>
         <div className="flex gap-4 text-3xl">
-          <FaGithub />
-          <FaTelegram />
-          <IoMail />
+          <a
+            href={linksOutside.github}
+            className="cursor-pointer"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={linksOutside.telegram}
+            className="cursor-pointer"
+          >
+            <FaTelegram />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={linksOutside.gmail}
+            className="cursor-pointer"
+          >
+            <IoMail />
+          </a>
         </div>
       </div>
     </section>
